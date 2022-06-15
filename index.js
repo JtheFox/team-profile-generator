4c881acec3ed42aeeb3234f7394a2d678fc97198
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-15 13:41:19 -0400

    update prompt delivery format

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ const fs = require('fs');
 const generateHTML = require('./utils/generateHTML');
 
 function getMemberPrompts(role) {
-    const roleQuestion = new Map(Object.entries({
-        manager: 'Enter the member\'s office number:',
-        engineer: 'Enter the member\'s GitHub username:',
-        intern: 'Enter the member\'s school name:'
-    }));
+    const roleQuestions = {
+        engineer: 'Enter the engineer\'s GitHub username:',
+        intern: 'Enter the intern\'s school name:'
+    };
 
    return [
         {
             type: 'input',
             name: 'name',
-            message: 'Enter the member\'s name:'
+            message: `Enter the ${role}'s name:`
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Enter the member\'s email:'
+            message: `Enter the ${role}'s email:`
         },
         {
             type: 'input',
             name: 'info',
-            message: roleQuestion.get(role)
+            message: roleQuestions[role]
         }
     ]
 }
@@ -32,41 +31,50 @@ function getMemberPrompts(role) {
 function writeHTML(html) {
     try {
         fs.writeFileSync('team.html', html)
-        console.log('Page successfully created at generated.html');
+        console.log('Page successfully created at team.html');
     } catch {
         console.log('Error writing to file, please try again.');
     }
 }
 
 async function init() {
-    const team = await inquirer.prompt([{
-        type: 'input',
-        name: 'count',
-        message: 'How many members are in your team?',
-        async validate(num) {
-            if (isNaN(num)) return 'Please enter a number'
-            num = parseInt(num);
-            if (Math.floor(num) > 0 && num < 7) return true;
-            else return 'Please enter a number 1-6';
+    var id = 1;
+    const team = [];
+    const manager = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the manager\'s name:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the manager\'s email:'
+        },
+        {
+            type: 'input',
+            name: 'info',
+            message: 'Enter the manager\'s office number:'
         }
-    }]);
-    team.members = [];
-    for (let i = 0; i < team.count; i++) {
-        let rolePrompt = await inquirer.prompt([{
+    ]);
+    team.push({id, role: 'manager', ...manager})
+    let role = true;
+    do {
+        id++;
+        rolePrompt = await inquirer.prompt({
             type: 'list',
             name: 'role',
-            message: `Choose team member ${i}'s role:`,
-            choices: [{name: 'Manager', value: 'manager'}, {name: 'Engineer', value: 'engineer'}, {name: 'Intern', value: 'intern'}]
-        }]);
-        let infoPrompt = await inquirer.prompt(getMemberPrompts(rolePrompt.role));
-        team.members.push({
-            id: i+1,
-            ...rolePrompt,
-            ...infoPrompt
-        })
-    }
+            message: 'Add a team member:',
+            choices: [{name: 'Engineer', value: 'engineer'}, {name: 'Intern', value: 'intern'}, {name: 'Done adding members', value: false}]
+        });
+        role = rolePrompt.role;
+        if (role) {
+            let member = await inquirer.prompt(getMemberPrompts(role));
+            team.push({id, role, ...member});
+        }
+    } while (role)
     console.log('Generating a webpage for your team...');
-    writeHTML(generateHTML(team.members));
+    writeHTML(generateHTML(team));
 }
 
 init();
